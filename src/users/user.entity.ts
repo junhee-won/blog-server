2f5acfc06d8ef88a0ae792f8b78e1c95e5135e3e
junhee-won <wwoon63@snu.ac.kr>
2022-11-16 17:06:41 +0900

    swagger 추가, db 연결

---
+++ src/users/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  user_id: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  user_type: boolean;
+}
