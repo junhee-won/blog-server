a88c82d52e1cd963c1bd1aba713024c8283034b3
junhee-won <wwoon63@snu.ac.kr>
2023-01-22 15:42:57 +0900

    [refactor] user module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User } from "./user.entity";
 
 @Injectable()
 export class UsersService {
@@ -12,7 +12,7 @@ export class UsersService {
 
   async findOne(user_id: string): Promise<User> {
     return this.usersRepository.findOneBy({
-      user_id: user_id,
+      user_id,
     });
   }
 }
